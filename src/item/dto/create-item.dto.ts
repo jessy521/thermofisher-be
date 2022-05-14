3f5909b18b640d2e345b8b91c42ecbda97b9f25e
jessy521 <jessykumarsarkar@gmail.com>
2022-05-14 20:50:23 +0530

    createdAt bug fixed

---
+++ src/item/dto/create-item.dto.ts
@@ -0,0 +1,73 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateItemDto {
+  @ApiPropertyOptional({
+    type: String,
+    description: 'enter the courceType',
+    default: '',
+  })
+  courceType: string;
+
+  @ApiPropertyOptional({
+    type: String,
+    description: 'enter the itemNo',
+    default: '',
+  })
+  itemNo: string;
+
+  @ApiPropertyOptional({
+    type: String,
+    description: 'enter the SFLMS-name',
+    default: '',
+  })
+  sflmsName: string;
+
+  @ApiPropertyOptional({
+    type: Boolean,
+    description: 'enter the PSG -certificate',
+    default: true,
+  })
+  psgCertificate: Boolean;
+
+  @ApiPropertyOptional({
+    type: Boolean,
+    description: 'enter the No-Certificate',
+    default: true,
+  })
+  noCertificate: Boolean;
+
+  @ApiPropertyOptional({
+    type: Boolean,
+    description: '',
+    default: true,
+  })
+  dx: Boolean;
+
+  @ApiPropertyOptional({
+    type: String,
+    description: 'enter the due date days no',
+    default: '',
+  })
+  dueDateDays: string;
+
+  @ApiPropertyOptional({
+    type: String,
+    description: 'enter the owner name',
+    default: '',
+  })
+  owner: string;
+
+  @ApiPropertyOptional({
+    type: String,
+    description: 'enter the curriculum Ids',
+    default: '',
+  })
+  curriculumId: [string];
+
+  @ApiPropertyOptional({
+    type: String,
+    description: 'enter the launch date',
+    default: '',
+  })
+  launchDate: Date;
+}
